86e7a6a2e073d78a1f629aac2263a37f3bac5583
BiLaL <97440505+E9N99@users.noreply.github.com>
2024-05-30 12:10:08 +0300

    Update config.py

--- config.py
+++ config.py
@@ -18,6 +18,6 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://proceed58:proceed58@cluster0.p5s9ym5.mongodb.net/?retryWrites=true&w=majority")
 
-DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 2000))
+
 
 # Chat id of a group for logging bot's activities
